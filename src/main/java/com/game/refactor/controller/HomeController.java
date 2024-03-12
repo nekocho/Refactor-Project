6d24d8f65a9990408e385afd16e7b26041b432ca
nekocho <zoewycho@gmail.com>
2024-03-12 12:30:37 +0000

    fixed dependency issues

--- src/main/java/com/game/refactor/controller/HomeController.java
+++ src/main/java/com/game/refactor/controller/HomeController.java
@@ -1,13 +1,12 @@
-package main.java.com.game.refactor.controller;
+package com.game.refactor.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/")
-    public RedirectView index() {
-        return new RedirectView(“/index”);
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Assuming index.html is your home page
     }
 }
