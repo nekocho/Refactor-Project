05b32e64a857cb70a43980181e70ed8b0af80814
Callum Bain <callumjbain@gmail.com>
2024-03-12 12:10:48 +0000

    setting up home controller

---
+++ src/main/java/com/game/refactor/controller/HomeController.java
@@ -0,0 +1,13 @@
+package main.java.com.game.refactor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public RedirectView index() {
+        return new RedirectView(“/index”);
+    }
+}
