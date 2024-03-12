d869a4b0ee66d2ebdae809fa456477707ee0afb5
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-03-12 16:45:09 +0000

    author names displaying in browser

--- src/main/java/com/game/refactor/controller/GameController.java
+++ src/main/java/com/game/refactor/controller/GameController.java
@@ -2,11 +2,15 @@ package com.game.refactor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import com.game.refactor.model.gameIntro.IntroText;
+
 
 @Controller
 public class GameController {
     @GetMapping("/game")
-    public String gamePage() {
+    public String gamePage(Model model) throws InterruptedException {
+        model.addAttribute("gameAuthor", IntroText.displayAuthorName());
         return "game";
     }
 }
--- src/main/java/com/game/refactor/model/gameIntro/IntroText.java
+++ src/main/java/com/game/refactor/model/gameIntro/IntroText.java
@@ -1,20 +1,26 @@
 package com.game.refactor.model.gameIntro;
 
-import com.game.refactor.model.textLogic.introTextPrinter;
 import com.game.refactor.model.textLogic.TextTyping;
 
 public class IntroText {
-    public static void displayGameName() throws InterruptedException {
+    public static String displayGameName() {
+        StringBuilder stringBuilder = new StringBuilder();
         // Print each line of the game name with no delay between lines
-        introTextPrinter.printLine("██████  ███████ ███████  █████   ██████ ████████  ██████  ██████ ");
-        introTextPrinter.printLine("██   ██ ██      ██      ██   ██ ██         ██    ██    ██ ██   ██");
-        introTextPrinter.printLine("██████  █████   █████   ███████ ██         ██    ██    ██ ██████  ");
-        introTextPrinter.printLine("██   ██ ██      ██      ██   ██ ██         ██    ██    ██ ██   ██ ");
-        introTextPrinter.printLine("██   ██ ███████ ██      ██   ██  ██████    ██     ██████  ██   ██");
+        stringBuilder.append("██████  ███████ ███████  █████   ██████ ████████  ██████  ██████ ");
+        stringBuilder.append("██   ██ ██      ██      ██   ██ ██         ██    ██    ██ ██   ██");
+        stringBuilder.append("██████  █████   █████   ███████ ██         ██    ██    ██ ██████  ");
+        stringBuilder.append("██   ██ ██      ██      ██   ██ ██         ██    ██    ██ ██   ██ ");
+        stringBuilder.append("██   ██ ███████ ██      ██   ██  ██████    ██     ██████  ██   ██");
+        return stringBuilder.toString();
     }
 
-    public static void displayAuthorName() throws InterruptedException {
-        TextTyping.printWithDelays("A game by, Callum, Matt, Sabirah & Zoe. Enjoy!", true);
+    public static void main(String[] args) {
+        String gameName = displayGameName();
+        System.out.println(gameName);
+    }
+
+    public static String displayAuthorName() throws InterruptedException {
+        return "A game by, Callum, Matt, Sabirah & Zoe. Enjoy!";
     }
 
 }
