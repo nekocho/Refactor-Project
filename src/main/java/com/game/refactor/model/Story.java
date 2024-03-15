3666296e25256eae1173a0728c97fa5a395cd4be
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-03-15 12:14:11 +0000

    minor text changes

--- src/main/java/com/game/refactor/model/Story.java
+++ src/main/java/com/game/refactor/model/Story.java
@@ -55,12 +55,12 @@ public class Story {
     }
 
     public void intro(){
-        setMainText("As the sun rises over the sprawling campus of Banana Inc., A new day dawns and a new journey begins. You find yourself standing at the gates of innovation, nerves tingling with anticipation as you take your first steps into the iconic headquarters.\nYou walk through the sleek, glass-fronted lobby, and your heart races as you approach the reception desk, where a friendly face welcomes you with a warm smile. He hands you your ID and ushers you through the gates to the lifts.\n\nWith determination in your eyes and ambition in your heart, you embark on your journey as an Banana apprentice. The road ahead may be challenging, but with perseverance and passion, you know that you have the opportunity to make a difference in the world of technology.\n\nWelcome to Banana Inc., Your adventure starts now...");
+        setMainText("As the sun rises over the sprawling campus of Banana Inc., A new day dawns and a new journey begins. You find yourself standing at the gates of innovation, nerves tingling with anticipation as you take your first steps into the iconic headquarters.\n\nYou walk through the sleek, glass-fronted lobby, and your heart races as you approach the reception desk, where a friendly face welcomes you with a warm smile. He hands you your ID and ushers you through the gates to the lifts.\n\nWith determination in your eyes and ambition in your heart, you embark on your journey as an Banana apprentice. The road ahead may be challenging, but with perseverance and passion, you know that you have the opportunity to make a difference in the world of technology.\n\nWelcome to Banana Inc., Your adventure starts now...");
         setChoices(Arrays.asList("Start Your Adventure"));
     }
 
     public void lifts(){
-        setMainText("You're standing by a set of very confusing lifts. Do you choose to follow: \n\nA) The tired person to your right? \nB) The friendly person to your left?");
-        setChoices(Arrays.asList("A) The tired person to your right", "B) The friendly person to your left"));
+        setMainText("You're standing by a set of very confusing lifts. Do you choose to follow: \n\nA) The tired person to your left? \nB) The friendly person to your right?");
+        setChoices(Arrays.asList("A) The tired person", "B) The friendly person"));
     }
 }
