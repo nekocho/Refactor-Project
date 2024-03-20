6777881eb1fe09779cb1c4331d6ccdf24f1bf53d
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-03-20 17:36:57 +0000

    minor text changes for readability

--- src/main/java/com/game/refactor/model/Story.java
+++ src/main/java/com/game/refactor/model/Story.java
@@ -135,7 +135,7 @@ public class Story {
 
     public void intro(){
         setImagePath("banana-inc-logo.png");
-        setMainText("As the sun rises over the sprawling campus of Banana Inc., a new day dawns and a new journey begins. You find yourself standing at the gates of innovation, nerves tingling with anticipation as you take your first steps into the iconic headquarters.\n\nYou walk through the sleek, glass-fronted lobby, and your heart races as you approach the reception desk, where a friendly face welcomes you with a warm smile. He hands you your ID and ushers you through the gates to the lifts.\n\nWith determination in your eyes and ambition in your heart, you embark on your journey as an Banana apprentice. The road ahead may be challenging, but with perseverance and passion, you know that you have the opportunity to make a difference in the world of technology.\n\nWelcome to Banana Inc. Your adventure starts now...");
+        setMainText("As the sun rises over the sprawling campus of Banana Inc., a new day dawns and a new journey begins. You find yourself standing at the gates of innovation, nerves tingling with anticipation as you take your first steps into the iconic headquarters.\n\nYou walk through the sleek, glass-fronted lobby, and your heart races as you approach the reception desk, where a friendly face welcomes you with a warm smile. He hands you your I.D. and ushers you through the gates to the lifts.\n\nWith determination in your eyes and ambition in your heart, you embark on your journey as an Banana apprentice. The road ahead may be challenging, but with perseverance and passion, you know that you have the opportunity to make a difference in the world of technology.\n\nWelcome to Banana Inc. Your adventure starts now...");
         setChoices(Arrays.asList("Start Your Adventure"));
         setSecretButton("Hidden Button");
     }
@@ -216,7 +216,7 @@ public class Story {
 
     public void bossTwo(){
         setImagePath(null);
-        setMainText("Well done, that's correct!\n\nLet's move onto your next question shall we?\n\nWhich AWS service is used for scalable computing capacity in the cloud?\n\nA) Amazon S3\nB) Amazon EC2\nC) Amazon RDS\nD) Amazon Lambda\n\nChoose the correct answer.");
+        setMainText("Well done, that's correct!\n\nLet's move onto your next question shall we?\n\nWhich Amazon Web Service is used for scalable computing capacity in the cloud?\n\nA) Amazon S3\nB) Amazon EC2\nC) Amazon RDS\nD) Amazon Lambda\n\nChoose the correct answer.");
         setChoices(Arrays.asList("A) Amazon S3", "B) Amazon EC2", "C) Amazon RDS", "D) Amazon Lambda"));
     }
 
