70829e1b2e8759624ee6fcc9bdb165705cc61de4
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-03-21 13:44:33 +0000

    removed some intro text

--- src/main/java/com/game/refactor/model/Story.java
+++ src/main/java/com/game/refactor/model/Story.java
@@ -135,7 +135,7 @@ public class Story {
 
     public void intro(){
         setImagePath("banana-inc-logo.png");
-        setMainText("As the sun rises over the sprawling campus of Banana Inc., a new day dawns and a new journey begins. You find yourself standing at the gates of innovation, nerves tingling with anticipation as you take your first steps into the iconic headquarters.\n\nYou walk through the sleek, glass-fronted lobby, and your heart races as you approach the reception desk, where a friendly face welcomes you with a warm smile. He hands you your I.D. and ushers you through the gates to the lifts.\n\nWith determination in your eyes and ambition in your heart, you embark on your journey as an Banana apprentice. The road ahead may be challenging, but with perseverance and passion, you know that you have the opportunity to make a difference in the world of technology.\n\nWelcome to Banana Inc. Your adventure starts now...");
+        setMainText("As the sun rises over the sprawling campus of Banana Inc., a new day dawns and a new journey begins. You find yourself standing at the gates of innovation, nerves tingling with anticipation as you take your first steps into the iconic headquarters.\n\nYou walk through the sleek, glass-fronted lobby, and your heart races as you approach the reception desk, where a friendly face welcomes you with a warm smile. He hands you your I.D. and ushers you through the gates to the lifts.\n\nWelcome to Banana Inc. Your adventure starts now...");
         setChoices(Arrays.asList("Start Your Adventure"));
         setSecretButton("Hidden Button");
     }
